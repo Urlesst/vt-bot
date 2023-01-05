9a02da67c682614ed5a0f1ba9ace873b8f331bfd
urlesst <urlesst@hotmail.com>
2023-01-04 20:44:59 -0500

    embed

--- main.py
+++ main.py
@@ -4,18 +4,12 @@ import config
 
 intents = discord.Intents.all() # Always use intents
 bot = commands.Bot(command_prefix="u!", intents=intents)
-presence = discord.Streaming(
-    name="Run", 
-    twitch_name="Someone", 
-    platform="Twitch", 
-    url="https://github.com", 
-    game="game"
-    ) # Bot presence
+presence = discord.Game('idk') # Bot presence
 
 # event
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.idle, activity=presence) # apply presence
+    await bot.change_presence(status=discord.Status.idle, activity=presence) # set presence
     print(f"Well done, Im {bot.user.name}. Lets gooo.") # very good 
     try:
         # synchronization for slash commands
@@ -28,16 +22,33 @@ async def on_ready():
 async def say(ctx, *, arg): 
     await ctx.send(arg)
 
-
 @bot.hybrid_command(name="avatar", description="Show any avatar")
 async def avatar(ctx, avatar: discord.Member=None):
+    embed = discord.Embed()
+    # In case not of a value
     if avatar == None:
-        await ctx.send(f'Avatar of **{ctx.author}** {ctx.author.display_avatar}')
+        embed.type = 'rich'
+        embed.colour = 0xED4245
+        embed.url = f'https://discord.com/users/{ctx.author.id}'
+        embed.title = f'Avatar of **{ctx.author}**'
+        embed.set_image(url=ctx.author.display_avatar)
+        await ctx.send(embed = embed) # Send embed
+    # Returns a value...
     else:
-        await ctx.send(f'Avatar of **{avatar}** {avatar.display_avatar}')
+        embed.type = 'rich'
+        embed.colour = 0xED4245
+        embed.url = f'https://discord.com/users/{avatar.id}'
+        embed.title = f'Avatar of **{avatar}**'
+        embed.set_image(url=avatar.display_avatar)
+        await ctx.send(embed = embed)
 
 @bot.hybrid_command(name="ping", description="Ping")
 async def ping(ctx):
-    await ctx.send(f'Currently the ping is {round(bot.latency * 1000)}ms')
+    embed = discord.Embed()
+    embed.type = 'rich'
+    embed.colour = 0xFEE75C
+    embed.description = f'Currently the ping is {round(bot.latency * 1000)}ms' 
+    embed.set_footer(text='Ping!', icon_url=None)
+    await ctx.send(embed = embed)
 
 bot.run(config.token) 
